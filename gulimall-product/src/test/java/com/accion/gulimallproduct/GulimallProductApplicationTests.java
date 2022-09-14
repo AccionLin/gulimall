1aef86e4a736be6eb03fc013d5c0385a15b55cb8
Accion <1058318272@QQ.com>
2022-09-14 15:44:59 +0800

    项目初始结构创建

---
+++ gulimall-product/src/test/java/com/accion/gulimallproduct/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accion.gulimallproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
